a7cd71dfb99c1de77c6017c0139813143117ec78
lucasalvess96 <lucasalves.57@hotmail.com>
2023-10-23 15:42:26 -0300

    feat: implementando mais testes

--- src/main/java/com/lombok/praticas/estudos/person/PersonService.java
+++ src/main/java/com/lombok/praticas/estudos/person/PersonService.java
@@ -3,7 +3,12 @@ package com.lombok.praticas.estudos.person;
 import com.lombok.praticas.estudos.person.Dto.PersonCreateDto;
 import com.lombok.praticas.estudos.person.comum.ValidateCpfUser;
 import com.lombok.praticas.estudos.person.comum.ValidateUser;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public record PersonService(PersonRepository personRepository) {
@@ -19,5 +24,17 @@ public record PersonService(PersonRepository personRepository) {
         person.setCpf(personCreateDto.cpf());
         
         return new PersonCreateDto(personRepository.save(person));
+    }
+    
+    public Page<PersonCreateDto> personListPagination(Pageable pageable) {
+        Page<PersonEntity> personEntityPage = personRepository.findAll(pageable);
+        return personEntityPage.map(PersonCreateDto::new);
+    }
+
+    public List<PersonCreateDto> personList() {
+        List<PersonEntity> personEntities = personRepository.findAll();
+        return personEntities.stream()
+                .map(PersonCreateDto::new)
+                .collect(Collectors.toList());
     }
 }
--- src/test/java/com/lombok/praticas/estudos/person/PersonControllerTest.java
+++ src/test/java/com/lombok/praticas/estudos/person/PersonControllerTest.java
@@ -6,8 +6,15 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+
+import java.util.Collections;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -19,6 +26,9 @@ class PersonControllerTest {
     
     @Mock
     private PersonService personService;
+    
+    @Mock
+    Pageable pageable;
     
     @InjectMocks
     private PersonController personController;
@@ -38,5 +48,56 @@ class PersonControllerTest {
         assertNotNull(response.getHeaders().getLocation());
         assertEquals("/create/1", response.getHeaders().getLocation().getPath());
         assertEquals(savedPersonCreateDto, response.getBody());
+    }
+
+    @Test
+    @DisplayName("Should test list method pagination")
+    void testPersonListPaginated() {
+        Pageable pageable = Pageable.unpaged();
+
+        PersonCreateDto personCreateDto = new PersonCreateDto(1L, "John Doe", "30", "123456789");
+        Page<PersonCreateDto> personCreateDtoPage = new PageImpl<>(Collections.singletonList(personCreateDto));
+
+        when(personService.personListPagination(pageable)).thenReturn(personCreateDtoPage);
+
+        ResponseEntity<Page<PersonCreateDto>> response = personController.list(pageable);
+
+        assertEquals(personCreateDtoPage, response.getBody());
+    }
+
+    @Test
+    @DisplayName("Should test list method with empty pagination")
+    void testListWithEmptyPagination() {
+        Page<PersonCreateDto> emptyPage = new PageImpl<>(Collections.emptyList());
+
+        when(personService.personListPagination(pageable)).thenReturn(emptyPage);
+
+        ResponseEntity<Page<PersonCreateDto>> response = personController.list(pageable);
+
+        assertEquals(emptyPage, response.getBody());
+    }
+
+    @Test
+    @DisplayName("Should test list method")
+    void testList() {
+        List<PersonCreateDto> personList = Collections.singletonList(new PersonCreateDto(1L, "John Doe", "30", "123456789"));
+
+        when(personService.personList()).thenReturn(personList);
+
+        ResponseEntity<List<PersonCreateDto>> response = personController.list();
+
+        assertEquals(personList, response.getBody());
+    }
+
+    @Test
+    @DisplayName("Should test list method with empty list")
+    void testListEmpty() {
+        List<PersonCreateDto> personList = Collections.emptyList();
+
+        when(personService.personList()).thenReturn(personList);
+
+        ResponseEntity<List<PersonCreateDto>> response = personController.list();
+
+        assertEquals(personList, response.getBody());        
     }
 }
