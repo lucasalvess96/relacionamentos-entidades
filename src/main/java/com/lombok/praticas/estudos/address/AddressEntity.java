51cba3489d115f1b4bac50f52f677c6660077dbc
lucasalvess96 <lucasalves.57@hotmail.com>
2023-11-28 22:16:02 -0300

    feat: implementando relacionamento OneToOne

---
+++ src/main/java/com/lombok/praticas/estudos/address/AddressEntity.java
@@ -0,0 +1,55 @@
+package com.lombok.praticas.estudos.address;
+
+import com.lombok.praticas.estudos.person.PersonEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AddressEntity {
+
+    @Id
+    @Column(name = "address_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String street;
+    private Long number;
+    private String city;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "person_id", unique = true)
+    private PersonEntity person;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof AddressEntity that)) return false;
+        return Objects.equals(getId(), that.getId()) && Objects.equals(getStreet(), that.getStreet())
+                && Objects.equals(getNumber(), that.getNumber()) && Objects.equals(getCity(), that.getCity())
+                && Objects.equals(getPerson(), that.getPerson());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getStreet(), getNumber(), getCity(), getPerson());
+    }
+
+    @Override
+    public String toString() {
+        return "AddressEntity{" +
+                "id=" + id +
+                ", street='" + street + '\'' +
+                ", number=" + number +
+                ", city='" + city + '\'' +
+                ", person=" + person +
+                '}';
+    }
+}
---
+++ src/main/java/com/lombok/praticas/estudos/persoon/Person.java
@@ -0,0 +1,50 @@
+package com.lombok.praticas.estudos.persoon;
+
+import com.lombok.praticas.estudos.address.AddressEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String cpf;
+
+    @OneToOne(mappedBy = "person", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
+    private AddressEntity address;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person person)) return false;
+        return Objects.equals(getId(), person.getId()) && Objects.equals(getName(), person.getName())
+                && Objects.equals(getCpf(), person.getCpf()) && Objects.equals(getAddress(), person.getAddress());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getCpf(), getAddress());
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", cpf='" + cpf + '\'' +
+                ", address=" + address +
+                '}';
+    }
+}
