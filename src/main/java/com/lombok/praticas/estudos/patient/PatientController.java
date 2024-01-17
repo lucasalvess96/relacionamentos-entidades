6475b8041a7d8661667f079c5f57b12729f445ec
lucasalvess96 <lucasalves.57@hotmail.com>
2024-01-16 21:25:54 -0300

    fix:swagger em Patient

--- src/main/java/com/lombok/praticas/estudos/patient/PatientController.java
+++ src/main/java/com/lombok/praticas/estudos/patient/PatientController.java
@@ -3,13 +3,6 @@ package com.lombok.praticas.estudos.patient;
 import com.lombok.praticas.estudos.patient.Dto.PatientDto;
 import com.lombok.praticas.estudos.patient.Dto.PatientSearchDto;
 import com.lombok.praticas.estudos.patient.swagger.PatientSwagger;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.enums.ParameterIn;
-import io.swagger.v3.oas.annotations.media.ArraySchema;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
@@ -40,9 +33,8 @@ public class PatientController implements PatientSwagger {
     public ResponseEntity<PatientDto> create(@RequestBody @Valid PatientDto patientDto) {
         PatientDto patientCreate = patientService.patientCreate(patientDto);
         return ResponseEntity.created(URI.create("/create/" + patientCreate.id()))
-                .body(patientService.patientCreate(patientDto));
+                .body(patientCreate);
     }
-
 
     @GetMapping("/pagination")
     @Override
@@ -51,54 +43,22 @@ public class PatientController implements PatientSwagger {
         return ResponseEntity.ok(patientService.patientPagination(pageable));
     }
 
-    @Operation(summary = "Listar todas as pessoas",
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "Lista de pessoas retornada com sucesso",
-                            content = @Content(
-                                    mediaType = "application/json",
-                                    array = @ArraySchema(schema = @Schema(implementation = PatientDto.class))
-                            )
-                    )
-            })
     @GetMapping("/list")
+    @Override
     public ResponseEntity<List<PatientDto>> patientList() {
         return ResponseEntity.ok(patientService.patientList());
     }
 
-    @Operation(summary = "Atualiza uma nova pessoa",
-            responses = {
-                    @ApiResponse(responseCode = "201", description = "Dados alterados com sucesso"),
-                    @ApiResponse(responseCode = "400", description = "Requisição inválida", content = @Content)
-            })
     @PutMapping("/update/{id}")
     @Transactional
-    public ResponseEntity<PatientDto> update(
-            @Parameter(
-                    name = "id",
-                    description = "ID da pessoa",
-                    in = ParameterIn.PATH,
-                    required = true,
-                    schema = @Schema(type = "integer", format = "int64", example = "1")
-            )
-            @PathVariable Long id, @RequestBody @Valid PatientDto patientDto) {
+    @Override
+    public ResponseEntity<PatientDto> update(@PathVariable Long id, @RequestBody @Valid PatientDto patientDto) {
         return ResponseEntity.ok(patientService.patientUpdate(id, patientDto));
     }
 
-    @Operation(summary = "Obter detalhes de uma pessoa pelo ID",
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "Detalhes da pessoa encontrados"),
-                    @ApiResponse(responseCode = "404", description = "Pessoa não encontrada", content = @Content)
-            })
     @GetMapping("/detail/{id}")
-    public ResponseEntity<PatientDto> detail(
-            @Parameter(
-                    name = "id",
-                    description = "ID da pessoa",
-                    in = ParameterIn.PATH,
-                    required = true,
-                    schema = @Schema(type = "integer", format = "int64", example = "1")
-            )
-            @PathVariable @Valid Long id) {
+    @Override
+    public ResponseEntity<PatientDto> detail(@PathVariable @Valid Long id) {
         Optional<PatientDto> patientDetailDto = patientService.patientDetail(id);
         return patientDetailDto.map(detailDto -> ResponseEntity.ok()
                         .body(detailDto))
@@ -106,53 +66,21 @@ public class PatientController implements PatientSwagger {
                         .build());
     }
 
-    @Operation(summary = "Pesquisar pessoas com paginação",
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "Página de resultados da pesquisa retornada com " +
-                            "sucesso",
-                            content = @Content(
-                                    mediaType = "application/json",
-                                    schema = @Schema(implementation = Page.class, allowableValues = "type" +
-                                            "=PatientSearchDto")
-                            )
-                    )
-            })
     @GetMapping("/search/pagination")
+    @Override
     public ResponseEntity<Page<PatientSearchDto>> pagedSearch(@RequestParam String name, Pageable pageable) {
         return ResponseEntity.ok(patientService.searchPatientPagination(name, pageable));
     }
 
-    @Operation(summary = "Pesquisar pessoas com lista",
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "Lita de resultados da pesquisa retornada com " +
-                            "sucesso",
-                            content = @Content(
-                                    mediaType = "application/json",
-                                    schema = @Schema(implementation = Page.class, allowableValues = "type" +
-                                            "=PatientSearchDto")
-                            )
-                    )
-            })
     @GetMapping("/search/list")
+    @Override
     public ResponseEntity<List<PatientSearchDto>> searchList(@RequestParam String name) {
         return ResponseEntity.ok(patientService.searchListPerson(name));
     }
 
-    @Operation(summary = "Excluir pessoa pelo ID",
-            responses = {
-                    @ApiResponse(responseCode = "204", description = "Pessoa excluída com sucesso"),
-                    @ApiResponse(responseCode = "404", description = "Pessoa não encontrada")
-            })
-    @DeleteMapping("delete/{id}")
-    public ResponseEntity<PatientEntity> delete(
-            @Parameter(
-                    name = "id",
-                    description = "ID da pessoa",
-                    in = ParameterIn.PATH,
-                    required = true,
-                    schema = @Schema(type = "integer", format = "int64", example = "1")
-            )
-            @PathVariable Long id) {
+    @DeleteMapping("/delete/{id}")
+    @Override
+    public ResponseEntity<PatientEntity> delete(@PathVariable Long id) {
         patientService.patientDelete(id);
         return ResponseEntity.noContent()
                 .build();
--- src/main/java/com/lombok/praticas/estudos/patient/swagger/PatientSwagger.java
+++ src/main/java/com/lombok/praticas/estudos/patient/swagger/PatientSwagger.java
@@ -2,22 +2,28 @@ package com.lombok.praticas.estudos.patient.swagger;
 
 import com.lombok.praticas.estudos.comun.ErroRequest;
 import com.lombok.praticas.estudos.patient.Dto.PatientDto;
+import com.lombok.praticas.estudos.patient.Dto.PatientSearchDto;
+import com.lombok.praticas.estudos.patient.PatientEntity;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 public interface PatientSwagger {
 
-    @Operation(summary = "Criar uma nova pessoa",
+    @Operation(summary = "Criar um novo paciente", description = "Cria um novo Paciente caso os campos estiverem " +
+            "preenchidos de forma válida",
             responses = {
-                    @ApiResponse(responseCode = "201", description = "Pessoa criada com sucesso"),
+                    @ApiResponse(responseCode = "201", description = "Paciente criado com sucesso"),
                     @ApiResponse(responseCode = "400", description = "Requisição inválida", content = @Content(
                             mediaType = "application/json",
                             schema = @Schema(implementation = ErroRequest.class),
@@ -48,9 +54,10 @@ public interface PatientSwagger {
                             schema = @Schema(implementation = PatientDto.class)
                     )
             )
-            @RequestBody @Valid PatientDto patientDto);
+            PatientDto patientDto);
 
-    @Operation(summary = "Listar pessoas com paginação",
+    @Operation(summary = "Listar paciente com paginação", description = "Lista quantidade de pacientes por " +
+            "página ",
             responses = {
                     @ApiResponse(responseCode = "200", description = "Lista de pessoas retornada com sucesso",
                             content = @Content(
@@ -69,4 +76,187 @@ public interface PatientSwagger {
                     )
             })
     ResponseEntity<Page<PatientDto>> patientPagination(Pageable pageable);
+
+    @Operation(summary = "Listar todas as pessoas", description = "Lista quantidade de pacientes",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Lista de pessoas retornada com sucesso",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    array = @ArraySchema(schema = @Schema(implementation = PatientDto.class))
+                            )
+                    ),
+                    @ApiResponse(responseCode = "500", description = "Não há informações na base dados",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ErroRequest.class),
+                                    examples = @ExampleObject(
+                                            value = "[]"
+                                    )
+                            )
+                    )
+            })
+    ResponseEntity<List<PatientDto>> patientList();
+
+    @Operation(summary = "Atualiza uma novo paciente", description = "método de atualizar",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Dados alterados com sucesso"),
+                    @ApiResponse(responseCode = "400", description = "Requisição inválida", content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErroRequest.class),
+                            examples = @ExampleObject(
+                                    value = "{\"timestamp\":\"2024-01-15T20:08:06\",\"status\":400," +
+                                            "\"error\":\"Erro na informação do campo\",\"message\": \"O campo NOME " +
+                                            "não deve está vazio, O campo NOME deve conter apenas letras\"}"
+                            )
+                    )),
+                    @ApiResponse(responseCode = "500", description = "Internal Server Error - Erro interno no " +
+                            "servidor ao tentar " +
+                            "processar requisicao",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ErroRequest.class),
+                                    examples = @ExampleObject(
+                                            value = "{\"timestamp\":\"2024-01-15T20:08:06\",\"status\":500," +
+                                                    "\"message\":\"Erro ao enviar informações\"}"
+                                    )
+                            )
+                    )
+            })
+    ResponseEntity<PatientDto> update(
+            @Parameter(
+                    name = "id",
+                    description = "ID da pessoa",
+                    in = ParameterIn.PATH,
+                    example = "1",
+                    required = true,
+                    schema = @Schema(type = "integer", format = "int64", example = "1")
+            )
+            Long id, PatientDto patientDto);
+
+    @Operation(summary = "Obter detalhes de um paciente pelo ID", description = "busca e informa detalhes do paciente",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Detalhes do paciente encontrado"),
+                    @ApiResponse(responseCode = "400", description = "Requisição inválida", content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErroRequest.class),
+                            examples = @ExampleObject(
+                                    value = "{\"timestamp\":\"2024-01-15T20:08:06\",\"status\":400," +
+                                            "\"error\":\"Erro na informação do campo\",\"message\": \"O campo NOME " +
+                                            "não deve está vazio, O campo NOME deve conter apenas letras\"}"
+                            )
+                    )),
+                    @ApiResponse(responseCode = "500", description = "Internal Server Error - Erro interno no " +
+                            "servidor ao tentar " +
+                            "processar requisicao",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ErroRequest.class),
+                                    examples = @ExampleObject(
+                                            value = "{\"timestamp\":\"2024-01-15T20:08:06\",\"status\":500," +
+                                                    "\"message\":\"Erro ao enviar informações\"}"
+                                    )
+                            )
+                    )
+            })
+    ResponseEntity<PatientDto> detail(
+            @Parameter(
+                    name = "id",
+                    description = "ID da pessoa",
+                    in = ParameterIn.PATH,
+                    example = "1",
+                    required = true,
+                    schema = @Schema(type = "integer", format = "int64", example = "1")
+            )
+            Long id
+    );
+
+    @Operation(summary = "Pesquisar Paciente com paginação", description = "buscar paciente informado de forma " +
+            "paginada",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Página de resultados da pesquisa retornada com " +
+                            "sucesso",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = Page.class, allowableValues = "type" +
+                                            "=PatientSearchDto")
+                            )
+                    ),
+                    @ApiResponse(responseCode = "400", description = "Requisição inválida", content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErroRequest.class),
+                            examples = @ExampleObject(
+                                    value = "{\"timestamp\":\"2024-01-15T20:08:06\",\"status\":400," +
+                                            "\"error\":\"Erro na informação do campo\",\"message\": \"O campo NOME " +
+                                            "não deve está vazio, O campo NOME deve conter apenas letras\"}"
+                            )
+                    )),
+            })
+    ResponseEntity<Page<PatientSearchDto>> pagedSearch(
+            @Parameter(
+                    name = "nome",
+                    description = "nome do paciente",
+                    in = ParameterIn.QUERY,
+                    example = "John",
+                    required = true,
+                    schema = @Schema(type = "integer", format = "int64", example = "John")
+            )
+            String name, Pageable pageable);
+
+    @Operation(summary = "Pesquisar pessoas com lista", description = "informa a listagem",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Lita de resultados da pesquisa retornada com " +
+                            "sucesso",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = Page.class, allowableValues = "type" +
+                                            "=PatientSearchDto")
+                            )
+                    )
+            })
+    ResponseEntity<List<PatientSearchDto>> searchList(
+            @Parameter(
+                    name = "nome",
+                    description = "nome do paciente",
+                    in = ParameterIn.QUERY,
+                    example = "Doe",
+                    required = true,
+                    schema = @Schema(type = "string", format = "int64", example = "Doe")
+            )
+            String name);
+
+    @Operation(summary = "Excluir paciente pelo ID", description = "exluir paciente buscado",
+            responses = {
+                    @ApiResponse(responseCode = "204", description = "Paciente excluída com sucesso"),
+                    @ApiResponse(responseCode = "400", description = "Requisição inválida", content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErroRequest.class),
+                            examples = @ExampleObject(
+                                    value = "{\"timestamp\":\"2024-01-15T20:08:06\",\"status\":400," +
+                                            "\"error\":\"Erro na informação do campo\",\"message\": \"O campo NOME " +
+                                            "não deve está vazio, O campo NOME deve conter apenas letras\"}"
+                            )
+                    )),
+                    @ApiResponse(responseCode = "500", description = "Internal Server Error - Erro interno no " +
+                            "servidor ao tentar " +
+                            "processar requisicao",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ErroRequest.class),
+                                    examples = @ExampleObject(
+                                            value = "{\"timestamp\":\"2024-01-15T20:08:06\",\"status\":500," +
+                                                    "\"message\":\"Erro ao enviar informações\"}"
+                                    )
+                            )
+                    )
+            })
+    ResponseEntity<PatientEntity> delete(
+            @Parameter(
+                    name = "id",
+                    description = "ID da pessoa",
+                    in = ParameterIn.PATH,
+                    example = "12",
+                    required = true,
+                    schema = @Schema(type = "integer", format = "int64", example = "12")
+            )
+            Long id);
 }
