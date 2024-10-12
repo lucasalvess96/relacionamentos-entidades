07cc5b8173770da059e8aeb7cc7e7d477c857974
lucasalvess96 <lucasalves.57@hotmail.com>
2024-10-11 23:38:18 -0300

    fix: ajustando bug do filtro

--- src/main/java/com/lombok/praticas/estudos/specification/product/ProductController.java
+++ src/main/java/com/lombok/praticas/estudos/specification/product/ProductController.java
@@ -3,6 +3,7 @@ package com.lombok.praticas.estudos.specification.product;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -27,7 +28,7 @@ public class ProductController {
 
     @GetMapping("/AllPageable")
     public ResponseEntity<Page<ProductDto>> getAllProducsPgeable(ProductFilter productFilter, @PageableDefault(sort =
-            "name") Pageable pageable) {
+            "name", direction = Sort.Direction.DESC) Pageable pageable) {
         return ResponseEntity.ok(productService.getAllProducts(productFilter, pageable));
     }
 }
--- src/main/java/com/lombok/praticas/estudos/specification/product/ProductFilter.java
+++ src/main/java/com/lombok/praticas/estudos/specification/product/ProductFilter.java
@@ -12,11 +12,23 @@ public class ProductFilter {
         return idProduct;
     }
 
+    public void setIdProduct(Long idProduct) {
+        this.idProduct = idProduct;
+    }
+
     public String getName() {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public Double getPrice() {
         return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
     }
 }
